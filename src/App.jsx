129497050b595ad3aa92aa5771f9e7bfba25108f
Mol43 <gaming060409>
2024-12-21 22:49:09 +0500

    addElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import Home from "./page/Home"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./page/Home";
+// import AnotherPage from "./page/AnotherPage"; 
 
 function App() {
   return (
-    <>
-<Home/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/another" element={<AnotherPage />} />  */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
