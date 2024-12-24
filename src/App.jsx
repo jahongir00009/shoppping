3bf8e43cd0c8e0f0b13ee1d3454c16304fd9fff8
jahongir00009 <xudoyberdiyevasunbula@gmail.com>
2024-12-24 07:40:17 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-=======
-import Home from "./page/Home"
 
+import Home from "./page/Home"
 function App() {
   return (
     <>
@@ -10,9 +8,5 @@ function App() {
   )
 }
 
-<<<<<<< HEAD
 export default App
->>>>>>> 9c90e4f199968557d3e29f26097f876d5f74a4a6
-=======
-export default App;
->>>>>>> 657f45ba2e5dc6093f989590d13f6c2067b18796
+
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import AddElement from '../components/AddElement'
 import Navber from '../components/ui/Navber'
 import Footer from '../components/ui/Footer'
+import Header from '../components/Header'
 
 function Home() {
   return (
     <>
       <Navber/>
+      <Header/>
       <AddElement />
       <Footer/>
     </>
