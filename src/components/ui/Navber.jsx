72696e6de6307b71107ace67557ec4ac3109a51c
mexa-bro <dantdm0106@gmail.com>
2024-12-24 15:05:09 +0500

    togri

--- src/components/ui/Navber.jsx
+++ src/components/ui/Navber.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AiOutlineHeart, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai'
 import { FiSearch } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 function Navber() {
   return (
@@ -12,10 +13,10 @@ function Navber() {
 
   
       <div className="flex space-x-6 text-lg">
-        <a href="/" className="hover:text-gray-700">Home</a>
-        <a href="/shop" className="hover:text-gray-700">Shop</a>
-        <a href="/about" className="hover:text-gray-700">About</a>
-        <a href="/contact" className="hover:text-gray-700">Contact</a>
+        <Link href="/" className="hover:text-gray-700">Home</Link>
+        <Link href="/shop" className="hover:text-gray-700">Shop</Link>
+        <Link href="/about" className="hover:text-gray-700">About</Link>
+        <Link href="/contact" className="hover:text-gray-700">Contact</Link>
       </div>
 
       <div className="flex items-center space-x-4 text-2xl">
